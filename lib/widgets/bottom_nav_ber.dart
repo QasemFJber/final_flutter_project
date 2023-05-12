9f469ee760c24b6c756337e4786e91f4b24cd16d
jaber <jaberqasem75@gmail.com>
2023-05-12 15:05:25 +0300

    BUILD PROJECT

---
+++ lib/widgets/bottom_nav_ber.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+
+class BottomNavigationBarWidget extends StatefulWidget {
+  @override
+  _BottomNavigationBarWidgetState createState() => _BottomNavigationBarWidgetState();
+}
+
+class _BottomNavigationBarWidgetState extends State<BottomNavigationBarWidget> {
+  int _selectedIndex = 0;
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: _getPage(_selectedIndex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.business),
+            label: 'Service',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.assignment),
+            label: 'Orders',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'User',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.more_horiz),
+            label: 'More',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.amber[800],
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+
+  Widget _getPage(int index) {
+    switch (index) {
+      case 0:
+        return Text('Service Page');
+      case 1:
+        return Text('Orders Page');
+      case 2:
+        return Text('User Page');
+      case 3:
+        return Text('More Page');
+      default:
+        return Container();
+    }
+  }
+}
