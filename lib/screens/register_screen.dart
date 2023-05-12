2c5860bb241609f00370dce9697b789683369b41
jaber <jaberqasem75@gmail.com>
2023-05-13 01:15:52 +0300

    TapBar Build Bottom NavBar Build

--- lib/screens/register_screen.dart
+++ lib/screens/register_screen.dart
@@ -13,7 +13,48 @@ class _RegiserPageState extends State<RegiserPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: RegisterWidget(),
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+              begin: AlignmentDirectional.topStart,
+              end: AlignmentDirectional.bottomEnd,
+              colors: [
+                Color(0xFF3672E0),
+                Color(0xFF6DC5FA),
+              ]),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Expanded(
+              flex: 1,
+                child: Padding(
+                  padding: const EdgeInsets.all(20),
+                  child: GestureDetector(
+                    onTap: (){
+                      Navigator.pushNamed(context,'/login_screen');
+                    },
+                    child: Image.asset('lib/images/back.png',height: 33,width: 33,),
+                  ),
+                ),
+            ),
+            Expanded(
+              flex: 3,
+              child: Container(
+                decoration: BoxDecoration(
+
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                    topRight: Radius.circular(20),
+                    topLeft: Radius.circular(20),
+                  ),
+                ),
+                child: RegisterWidget(),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
