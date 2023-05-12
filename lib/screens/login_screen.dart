c4e9cc8dd5999d49a048534e6cc9b0408fd94ec6
jaber <jaberqasem75@gmail.com>
2023-05-12 14:51:32 +0300

    BUILD PROJECT

---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+              begin: AlignmentDirectional.topStart,
+              end: AlignmentDirectional.bottomEnd,
+              colors: [
+                Color(0xFF3672E0),
+                Color(0xFF6DC5FA),
+              ]),
+        ),
+        child: Column(
+          children: [
+            Expanded(
+              flex: 1,
+              child: Container(
+                child: Center(
+                  child: Image.asset('lib/images/layer_3.png'),
+                ),
+                color: Colors.cyan,
+              ),
+            ),
+            Expanded(
+              flex: 3,
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                    topRight: Radius.circular(20),
+                    topLeft: Radius.circular(20),
+                  ),
+                ),
+                child: TabBar(
+                  tabs: [
+                    Tab(
+                      
+                      text: 'CUSTOMER',
+                    ),
+                    Tab(
+                      text: 'SERVICE PROVIDER',
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
