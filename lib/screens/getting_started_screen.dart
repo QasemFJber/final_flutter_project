15cbc171a7d86333e5c08df355418c50bd2ff79a
jaber <jaberqasem75@gmail.com>
2023-05-12 15:05:31 +0300

    BUILD PROJECT

---
+++ lib/screens/getting_started_screen.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+import '../widgets/bottom_nav_ber.dart';
+
+class Getting_Started_Screen extends StatefulWidget {
+  const Getting_Started_Screen({Key? key}) : super(key: key);
+
+  @override
+  State<Getting_Started_Screen> createState() => _Getting_Started_ScreenState();
+}
+
+class _Getting_Started_ScreenState extends State<Getting_Started_Screen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Your App Name'),
+      ),
+      body: BottomNavigationBarWidget(),
+    );
+  }
+}
