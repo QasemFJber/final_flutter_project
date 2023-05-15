4916d23bea62cccf80b7360b6e9f1584e4b2dd8f
jaber <jaberqasem75@gmail.com>
2023-05-15 22:49:43 +0300

    Improved user interfaces and the latest modifications

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -8,30 +8,30 @@ class LoginPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        decoration: BoxDecoration(
-          gradient: LinearGradient(
-              begin: AlignmentDirectional.topStart,
-              end: AlignmentDirectional.bottomEnd,
-              colors: [
-                Color(0xFF3672E0),
-                Color(0xFF6DC5FA),
-              ]),
-        ),
         child: Column(
           children: [
             Expanded(
               flex: 1,
               child: Container(
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                      begin: AlignmentDirectional.topEnd,
+                      end: AlignmentDirectional.topStart,
+                      colors: [
+                        Color(0xFF6DC5FA),
+                        Color(0xFF346EDF),
+                      ]),
+                ),
                 child: Center(
                   child: Image.asset('lib/images/layer_3.png'),
                 ),
-                color: Colors.cyan,
               ),
             ),
             Expanded(
               flex: 3,
               child: Container(
                 decoration: BoxDecoration(
+
                   color: Colors.white,
                   borderRadius: BorderRadius.only(
                     topRight: Radius.circular(20),
