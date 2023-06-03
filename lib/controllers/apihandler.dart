3b71af5a612227317eaf31b2282557a7bec6e109
jaber <jaberqasem75@gmail.com>
2023-06-03 10:46:16 +0300

    Complete all requirements, add multilingual feature, and link with API

--- lib/controllers/apihandler.dart
+++ lib/controllers/apihandler.dart
@@ -1,28 +1,29 @@
-import 'dart:convert';
-import "package:http/http.dart" as http;
+// import 'dart:convert';
+// import "package:http/http.dart" as http;
+//
+// class APIHandler {
+//   final String baseUrl = 'https://api.example.com';
+//   Future<dynamic> fetchData(String endpoint) async {
+//     final response = await http.get(Uri.parse('$baseUrl/$endpoint'));
+//
+//     if (response.statusCode == 200) {
+//       return json.decode(response.body);
+//     } else {
+//       throw Exception('Failed to load data from API');
+//     }
+//   }
 
-class APIHandler {
-  final String baseUrl = 'https://api.example.com';
-  Future<dynamic> fetchData(String endpoint) async {
-    final response = await http.get(Uri.parse('$baseUrl/$endpoint'));
-
-    if (response.statusCode == 200) {
-      return json.decode(response.body);
-    } else {
-      throw Exception('Failed to load data from API');
-    }
-  }
-  Future<dynamic> postData(String endpoint, dynamic data) async {
-    final response = await http.post(
-      Uri.parse('$baseUrl/$endpoint'),
-      body: json.encode(data),
-      headers: {'Content-Type': 'application/json'},
-    );
-
-    if (response.statusCode == 201) {
-      return json.decode(response.body);
-    } else {
-      throw Exception('Failed to post data to API');
-    }
-  }
-}
+//   Future<dynamic> postData(String endpoint, dynamic data) async {
+//     final response = await http.post(
+//       Uri.parse('$baseUrl/$endpoint'),
+//       body: json.encode(data),
+//       headers: {'Content-Type': 'application/json'},
+//     );
+//
+//     if (response.statusCode == 201) {
+//       return json.decode(response.body);
+//     } else {
+//       throw Exception('Failed to post data to API');
+//     }
+//   }
+// }
