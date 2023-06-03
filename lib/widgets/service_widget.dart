2ba271d8d1ef0cd5afccf729c6b9275e475a1a2f
jaber <jaberqasem75@gmail.com>
2023-06-03 10:47:09 +0300

    Complete all requirements, add multilingual feature, and link with API

--- lib/widgets/service_widget.dart
+++ lib/widgets/service_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
 class ServiceScreen extends StatelessWidget {
   final List<String> images = [
@@ -16,8 +17,7 @@ class ServiceScreen extends StatelessWidget {
     'lib/images/ds - Copy.png',
   ];
 
-
-  final List<String> texts = [  'Carpenter'];
+  final List<String> texts = ['Carpenter'];
 
   @override
   Widget build(BuildContext context) {
@@ -36,24 +36,50 @@ class ServiceScreen extends StatelessWidget {
           ),
         ),
         actions: [
-          IconButton(
-            icon: Image.asset("lib/images/Shape.png"),
-            onPressed: () {
-              // تنفيذ إجراء عند النقر على أيقونة الإشعارات
-            },
-          ),
-        ],
+      IconButton(
+      icon: Image.asset("lib/images/Shape.png"),
+      onPressed: () {
+        Fluttertoast.showToast(
+          msg: "تم اضافة الخدمة بنجاح ",
+          toastLength: Toast.LENGTH_SHORT,
+        );
+        Fluttertoast.showToast(
+          msg: "شكرا لتعاملك مع تطبيق خدمات",
+          toastLength: Toast.LENGTH_LONG,
+        );
+      },
+      ),
+      ],
       ),
       body: Container(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            Image.asset('lib/images/Background.png',fit: BoxFit.fitWidth,),
+            Stack(
+              fit: StackFit.passthrough,
+              children: [
+                Image.asset(
+                  'lib/images/Background.png',
+                  fit: BoxFit.fitWidth,
+                ),
+                Container(
+                  margin: EdgeInsets.all(33),
+                  child: Card(
+                    color: Colors.white,
+                    child: Container(
+                      width: 222,
+                      height: 188,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+
             SizedBox(height: 10),
             Center(
               child: Text(
                 'Select Service',
-                style: TextStyle(fontSize: 24,color: Colors.blue),
+                style: TextStyle(fontSize: 24, color: Colors.blue),
               ),
             ),
             SizedBox(height: 16),
@@ -61,42 +87,38 @@ class ServiceScreen extends StatelessWidget {
               child: Padding(
                 padding: const EdgeInsets.all(16),
                 child: GridView.count(
-                    crossAxisCount: 3,
-                    children:
-                    List.generate(12, (index) {
-                      return GestureDetector(
-                        onTap: (){
-
-                          Navigator.pushNamed(context, '/choice_file_screen');
-                        },
-                        child: Card(
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(8),
-                            side: BorderSide(
-                              color: Colors.pink[100]!,
-                              width: 1,
-                            ),
+                  crossAxisCount: 3,
+                  children: List.generate(12, (index) {
+                    return GestureDetector(
+                      onTap: () {
+                        Navigator.pushNamed(context, '/choice_file_screen');
+                      },
+                      child: Card(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8),
+                          side: BorderSide(
+                            color: Colors.pink[100]!,
+                            width: 1,
                           ),
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              Image.asset(
-                                images[index % images.length],
-                                width: 64,
-                                height: 64,
-                              ),
-                              SizedBox(height: 8),
-                              Text(
-                                texts[index % texts.length],
-                                style: TextStyle(color: Colors.blue),
-                              ),
-                            ],
-                          ),
-
                         ),
-                      );
-                    }),
-
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Image.asset(
+                              images[index % images.length],
+                              width: 64,
+                              height: 64,
+                            ),
+                            SizedBox(height: 8),
+                            Text(
+                              texts[index % texts.length],
+                              style: TextStyle(color: Colors.blue),
+                            ),
+                          ],
+                        ),
+                      ),
+                    );
+                  }),
                 ),
               ),
             ),
